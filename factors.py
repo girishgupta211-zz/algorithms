cf00ebf2c02ab6839f9bafe659ea3565b78265c4
Girish Gupta <girishgargdce@gmail.com>
2018-07-23 09:01:38 +0530

    Update factors.py
    Made more readbale

--- factors.py
+++ factors.py
@@ -1,25 +1,16 @@
-# def factors(n):    
-#     return reduce(list.__add__, 
-#                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))
-
-
-def factors(n):    
+def factors(n):
     l1, l2 = [], []
-    for i in range(1, int(n ** 0.5) + 1):
-    	print i
-        q,r = n//i, n%i     # Alter: divmod() fn can be used.
-        if r == 0:
-            l1.append(i) 
-            l2.append(q)    # q's obtained are decreasing.
-    if l1[-1] == l2[-1]:    # To avoid duplication of the possible factor sqrt(n)
-        l1.pop()
+    square_root = int(n ** 0.5)
+    for i in range(1, square_root + 1):
+        quotient, reminder = n // i, n % i  # Alter: divmod() fn can be used.
+        if reminder == 0:
+            l1.append(i)
+            # Check if both quotient and i are not equal to avoid duplicate numbers
+            if i != quotient:
+                l2.append(quotient)  # quotients obtained are decreasing.
     l2.reverse()
     return l1 + l2
 
 
 facts = factors(10)
-
-if(len(facts) <= 3):
-	print facts[0]
-else:
-	print 0
+print(facts)
