a693c5120013fbd4ae156379527ac2bf9c39ce53
Girish Gupta <girishgargdce@gmail.com>
2018-07-23 08:42:57 +0530

    Update bitManupulation.py

--- bitManupulation.py
+++ bitManupulation.py
@@ -1,26 +1,19 @@
-# def bitManupulation:
-
+# Get number of 1s in binary representation of a number
 a = 1023
 count = 0
 while a:
-	count = count + (a&1)
-	a = (a>>1)
-	# print a
-	
+    count = count + (a & 1)
+    # Right shift
+    a = (a >> 1)
+print(count)
 
-# print count
-
+# Get number with count in a given array
 arr = [12, 1, 12, 3, 12, 1, 1, 2, 3, 3]
-dict = {}
+num_dict = {}
 for elm in arr:
-	if( elm in dict):
-		dict[elm] += 1
-	else:
-		dict[elm] = 1
+    if elm in num_dict.keys():
+        num_dict[elm] += 1
+    else:
+        num_dict[elm] = 1
 
-for key in dict:
-	if(dict[key] == 1):
-		print key
-		
-
-print dict	
+print(num_dict)
