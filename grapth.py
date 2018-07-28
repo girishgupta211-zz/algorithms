7894cca15f9bdbbd095ff295dc0c90c4251d5207
Girish Gupta <girish@supply.ai>
2018-07-28 12:08:52 +0530

    added function to generate_graph and find_path

--- grapth.py
+++ grapth.py
@@ -2,49 +2,49 @@
 # vertex. BFS(int s) traverses vertices reachable
 # from s.
 from collections import defaultdict
- 
+
+
 # This class represents a directed graph using adjacency
 # list representation
 class Graph:
- 
+
     # Constructor
     def __init__(self):
- 
+
         # default dictionary to store graph
         self.graph = defaultdict(list)
- 
+
     # function to add an edge to graph
-    def addEdge(self,u,v):
-        print ("u: " + str(u))
-        print ("v: " + str(v))
+    def addEdge(self, u, v):
+        print("u: " + str(u))
+        print("v: " + str(v))
         self.graph[u].append(v)
- 
+
     # Function to print a BFS of graph
     def BFS(self, s):
- 
+
         # Mark all the vertices as not visited
-        visited = [False]*(len(self.graph))
-        print (visited)
- 
+        visited = [False] * (len(self.graph))
+        print(visited)
+
         # Create a queue for BFS
         queue = []
- 
+
         # Mark the source node as visited and enqueue it
         queue.append(s)
         visited[s] = True
-        print (visited)
+        print(visited)
         # print queue
         # print s
- 
-        while queue:
- 
-            # Dequeue a vertex from queue and print it
-            print (queue)
-            s = queue.pop(0)
-            print (s,)
-            print ("queue" + str(queue))
 
- 
+        while queue:
+
+            # Dequeue a vertex from queue and print it
+            print(queue)
+            s = queue.pop(0)
+            print(s, )
+            print("queue" + str(queue))
+
             # Get all adjacent vertices of the dequeued
             # vertex s. If a adjacent has not been visited,
             # then mark it visited and enqueue it
@@ -52,8 +52,9 @@ class Graph:
                 if visited[i] == False:
                     queue.append(i)
                     visited[i] = True
- 
- # Driver code
+
+
+# Driver code
 # Create a graph given in the above diagram
 g = Graph()
 g.addEdge(0, 1)
@@ -62,6 +63,38 @@ g.addEdge(1, 2)
 g.addEdge(2, 0)
 g.addEdge(2, 3)
 g.addEdge(3, 3)
-print ("Following is Breadth First Traversal (starting from vertex 2)")
+print("Following is Breadth First Traversal (starting from vertex 2)")
 g.BFS(2)
 print(g.graph)
+
+
+def generate_edges(graph):
+    edges = []
+    # for each node in graph
+    for node in graph:
+        # print(node)
+        # for each neighbour node of a single node
+        for neighbour in graph[node]:
+            # if edge exists then append
+            edges.append((node, neighbour))
+    return edges
+
+
+print(generate_edges(g.graph))
+
+
+# function to find path
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath:
+                return newpath
+            return None
+
+
+# Driver function call to print the path
+print(find_path(g.graph, 4, 6))
