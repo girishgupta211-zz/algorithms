37bfbbb7cf5db78f9e0336ee5c42883847c364f0
Girish Gupta <girish@hotstar.com>
2019-07-09 11:08:49 +0530

    added minimum distance

--- graph_bfs.py
+++ graph_bfs.py
@@ -3,6 +3,10 @@ import queue
 
 
 class Graph:
+    """
+    Graph traversal algorithm using BFS
+    """
+
     def __init__(self):
         self.graph = defaultdict(list)
 
@@ -33,12 +37,12 @@ class Graph:
         for vertex in self.graph:
             for neighbour in self.graph[vertex]:
                 edges.append((vertex, neighbour))
-                # print("{} -> {}".format(vertex, neighbour))
-        print(edges)
+                print("{} -> {}".format(vertex, neighbour))
+        # print(edges)
         return edges
 
-    def find_distance(self, vertex, destination):
-        visited, distances = dict(), dict()
+    def find_distance(self, vertex):
+        visited, distances, nodes = dict(), dict(), dict()
         for key in self.graph.keys():
             visited[key] = False
             distances[key] = -1
@@ -55,16 +59,47 @@ class Graph:
                     distances[neighbour] = distances[elm] + 1
                     q.put(neighbour)
                     visited[neighbour] = True
-        print(distances[destination])
+                    nodes[neighbour] = elm
+        return distances, nodes
+
+    # TODO this is not gining the shortest path
+    def find_path(self, start, end, path=[]):
+        path = path + [start]
+        # path = path.append([start])
+        if start == end:
+            return path
+        for node in self.graph[start]:
+            if node not in path:
+                new_path = self.find_path(node, end, path)
+                if new_path:
+                    return new_path
+                return None
 
 
-graph = Graph()
-graph.add_edge(0, 1)
-graph.add_edge(0, 2)
-graph.add_edge(1, 2)
-graph.add_edge(1, 3)
-graph.add_edge(2, 4)
-graph.add_edge(4, 5)
-graph.BFS(0)
-graph.generate_edges()
-graph.find_distance(1, 4)
+if __name__ == '__main__':
+    graph = Graph()
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(2, 4)
+    graph.add_edge(4, 5)
+    graph.BFS(0)
+    graph.generate_edges()
+    start_node = 1
+    destination_node = 4
+    distances_from_start, parent_nodes = graph.find_distance(start_node)
+    # shortest_path = graph.find_path(1, 4)
+    min_distance = distances_from_start[destination_node]
+    print("minimum distance between {} and {}: {}".
+          format(start_node, destination_node, min_distance))
+
+    crawl = destination_node
+    shortest_path = [destination_node]
+    while crawl != start_node:
+        parent = parent_nodes[crawl]
+        shortest_path.insert(0, parent)
+        crawl = parent
+    print("shortest path between {} and {}: {}".
+          format(start_node, destination_node,
+                 "->".join(map(str, shortest_path))))
