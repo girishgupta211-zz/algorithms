78dc2ada16657d624fc2940730bf3ff7abb038df
Girish Gupta <girish@hotstar.com>
2019-08-26 23:05:34 +0530

    added recursion method for max key strokes

--- dynamic_programming/max_key_stroke.py
+++ dynamic_programming/max_key_stroke.py
@@ -3,14 +3,31 @@
 # If you are allowed to press keys of this special keyboard N times,
 # write a program which calculates maximum numbers of A's possible ~~~~Asked in : Google, Adobe, NetApp
 
+
 def max_key_strokes(n):
-    dp = [0] * (n + 1)
+    lookup = [0] * (n + 1)
     for i in range(n):
-        dp[i + 1] = max(dp[i + 1], dp[i] + 1)  # press a
+        lookup[i + 1] = max(lookup[i + 1], lookup[i] + 1)  # press a
         for j in range(i + 3, min(i + 7, n + 1)):
-            dp[j] = max(dp[j], dp[i] * (j - i - 1))  # press select all, copy, paste x (j-i-1)
-    return dp[n]
+            lookup[j] = max(lookup[j], lookup[i] * (j - i - 1))  # press select all, copy, paste x (j-i-1)
+    return lookup[n]
 
 
 print(max_key_strokes(24))
 print(max_key_strokes(6))
+
+
+def find_max_key_strokes(n):
+    if n <= 6:
+        return n
+
+    max_so_far = 0
+    for b in range(n - 3, 0, -1):
+        curr = (n - b - 1) * find_max_key_strokes(b)
+        max_so_far = max(max_so_far, curr)
+
+    return max_so_far
+
+
+print(find_max_key_strokes(24))
+print(find_max_key_strokes(6))
